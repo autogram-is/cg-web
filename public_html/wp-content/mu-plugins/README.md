de747a13720b239d9fadcdb03408a3611e05db4b
Jeff Eaton <jeff@eaton.fyi>
2024-10-02 14:02:15 -0500

    Adding stubbed CG Core plugin

---
+++ public_html/wp-content/mu-plugins/README.md
@@ -0,0 +1,5 @@
+# Must-Use Plugins
+
+This directory contains plugins that will *always* be enabled by Wordpress.
+
+These plugins will *not* appear in the plugin dashboard.
---
+++ public_html/wp-content/plugins/cg_core/cg_core.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package CG Core
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: CG Core
+Plugin URI: http://github.com/autogram-is/cg-web
+Description: Defines custom post types and core site settings.
+Author: Autogram
+Version: 1.0.0
+Author URI: http://autogram.is/
+*/
+
+// Custom post type definitions
+require_once __DIR__ .'/post_types.php';
