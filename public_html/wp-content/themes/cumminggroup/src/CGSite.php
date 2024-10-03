44e0b8097c32a43bdb94c1889daf365b22d38655
Jeff Eaton <jeff@eaton.fyi>
2024-10-03 17:40:08 -0500

    Commenting out several snippets of demo code.

--- public_html/wp-content/themes/cumminggroup/src/CGSite.php
+++ public_html/wp-content/themes/cumminggroup/src/CGSite.php
@@ -38,9 +38,9 @@ class CGSite extends Site {
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
 	public function add_to_context( $context ) {
-		$context['foo']   = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::context();';
+		// $context['foo']   = 'bar';
+		// $context['stuff'] = 'I am a value set in your functions.php file';
+		// $context['notes'] = 'These values are available everytime you call Timber::context();';
 		$context['menu']  = Timber::get_menu();
 		$context['site']  = $this;
 
