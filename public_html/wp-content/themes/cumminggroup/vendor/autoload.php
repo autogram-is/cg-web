1842781a2c2b0ed7844ef14650f682847d666bf0
Jeff Eaton <jeff@eaton.fyi>
2024-10-08 11:15:48 -0500

    Rolling in the vendor directory for now

--- public_html/wp-content/themes/cumminggroup/vendor/autoload.php
+++ public_html/wp-content/themes/cumminggroup/vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit02274576acc8961c3cbe36acaf88877d::getLoader();
+return ComposerAutoloaderInit45d539327a396619f1041b04bd0610d3::getLoader();
