9ba43e8e64b6db6442da63ac0f6839be01ae1539
Jeff Eaton <jeff@eaton.fyi>
2024-10-22 20:39:53 -0500

    Additional type tweaks

--- public_html/wp-content/plugins/cg-migrate/posts.php
+++ public_html/wp-content/plugins/cg-migrate/posts.php
@@ -7,5 +7,14 @@
 add_filter( 'wp_import_post_data_raw', 'cg_migrate_post' );
 
 function cg_migrate_post($post) {
+  if ($post->post_type == 'avada_portfolio') {
+    return cg_migrate_portfolio_item($post);
+  }
+
+  return $post;
+}
+
+function cg_migrate_portfolio_item($post) {
+  $post->type = 'cg_portfolio';
   return $post;
 }
