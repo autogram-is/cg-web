0ea6dfcc35569e2d7086f94f8aca88cd49643272
Jeff Eaton <jeff@eaton.fyi>
2024-11-06 17:09:20 -0600

    Testing Block Manager to override Gutenberg block settings.

---
+++ public_html/wp-content/plugins/block-manager/build/block-manager-admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-library', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f50e4578546d80fe75ed');
---
+++ public_html/wp-content/plugins/block-manager/build/block-manager.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-blocks', 'wp-dom-ready', 'wp-hooks'), 'version' => '221224ee4a0e4fe9c64e');
