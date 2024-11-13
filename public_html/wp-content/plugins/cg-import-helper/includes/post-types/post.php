16607e62e3cd7efdcd16d9f412844ae47d25b490
Jeff Eaton <jeff@eaton.fyi>
2024-11-13 11:20:24 -0600

    TODO notes for post migration

--- public_html/wp-content/plugins/cg-import-helper/includes/post-types/post.php
+++ public_html/wp-content/plugins/cg-import-helper/includes/post-types/post.php
@@ -1,5 +1,12 @@
 <?php
 
+// We need special handling for several categories of posts:
+// 1. 'Events Hosted by Others' - Turn them into `cg_event` posts.
+// 2. 'Podcast episodes' - Right now we have a cg_podcast type, but may just treat it as a
+//    special case of news/thought leadership.
+// 3. 'Case Study' - in-depth writeups of several projects. Skip them and manually move the
+//    data to the appropriate Project post.
+
 function cgih_preprocess_raw_post($postdata) {
   // This covers news and other posts.
 
