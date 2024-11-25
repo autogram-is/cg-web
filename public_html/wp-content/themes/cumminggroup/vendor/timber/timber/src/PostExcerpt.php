7cb5a9655d438b34cf5b491d9b6a6ae10592cf20
Mat Marquis <mat@matmarquis.com>
2024-11-25 15:42:36 -0500

    Basic Event index/article page layouts.

--- public_html/wp-content/themes/cumminggroup/vendor/timber/timber/src/PostExcerpt.php
+++ public_html/wp-content/themes/cumminggroup/vendor/timber/timber/src/PostExcerpt.php
@@ -159,7 +159,7 @@ class PostExcerpt implements Stringable
             'end' => '&hellip;',
             'force' => false,
             'strip' => true,
-            'read_more' => 'Read More',
+            'read_more' => false,
             'always_add_read_more' => false,
             'always_add_end' => false,
         ];
