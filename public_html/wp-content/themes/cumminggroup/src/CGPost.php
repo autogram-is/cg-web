7345aa83de4aeb93241c7e0a2eb097d11d60e697
Jeff Eaton <jeff@eaton.fyi>
2024-12-04 16:30:52 -0600

    CGPost updates

--- public_html/wp-content/themes/cumminggroup/src/CGPost.php
+++ public_html/wp-content/themes/cumminggroup/src/CGPost.php
@@ -28,75 +28,35 @@ class CGPost extends Post {
 	 *
 	 * @return \Timber\PostCollectionInterface
 	 */
-	public function regions() {
-		if (isset($this->regions)) {
-			return $this->regions;
-		}
-
-		$this->regions = Timber::get_posts($this->meta('regions'));
-
-		return $this->regions;
-	}
+	public function regions() { return $this->_related('regions'); }
 
 	/**
 	 * Gets office pages connected to the current post.
 	 *
 	 * @return \Timber\PostCollectionInterface
 	 */
-	public function offices() {
-		if (isset($this->offices)) {
-			return $this->offices;
-		}
-
-		$this->offices = Timber::get_posts($this->meta('offices'));
-
-		return $this->offices;
-	}
+	public function offices() { return $this->_related('offices'); }
 
 	/**
 	 * Gets sector pages connected to the current post.
 	 *
 	 * @return \Timber\PostCollectionInterface
 	 */
-	public function sectors() {
-		if (isset($this->sectors)) {
-			return $this->sectors;
-		}
-
-		$this->sectors = Timber::get_posts($this->meta('sectors'));
-
-		return $this->sectors;
-	}
+	public function sectors() { return $this->_related('sectors'); }
 
 	/**
-	 * Gets service pages connected to the current post.
+	 * Gets services connected to the current post.
 	 *
 	 * @return \Timber\PostCollectionInterface
 	 */
-	public function services() {
-		if (isset($this->services)) {
-			return $this->services;
-		}
-
-		$this->services = Timber::get_posts($this->meta('services'));
-
-		return $this->services;
-	}
+	public function services() { return $this->_related('services'); }
 
 	/**
 	 * Gets projects connected to the current post.
 	 *
 	 * @return \Timber\PostCollectionInterface
 	 */
-	public function projects() {
-		if (isset($this->projects)) {
-			return $this->projects;
-		}
-
-		$this->projects = Timber::get_posts($this->meta('projects'));
-
-		return $this->projects;
-	}
+	public function projects() { return $this->_related('projects'); }
 
 	/**
 	 * On content with related portfolio-items (events, posts, etc), returns related portfolio pages.
@@ -105,20 +65,24 @@ class CGPost extends Post {
 	 * @return \Timber\PostCollectionInterface
 	 */
 	public function related() {
-		if (isset($this->related_pages)) {
-			return $this->related_pages;
-		} else {
-			$this->related_pages = Timber::get_posts($this->meta('related_pages'));
-			return $this->related_pages;
-		}
-
-		if (isset($this->related_news)) {
-			return $this->related_news;
-		} else {
-			$this->related_news = Timber::get_posts($this->meta('related_news'));
-			return $this->related_news;
-		}
-
 		return array();
+	}
+
+	/**
+	 * Returns the portfolio pages a news item is related to, formatted such that they can be passed
+	 * to `links` output functions.
+	 */
+	public function portfolio_tags() {
+		return array();
+	}
+
+	private function _related($field_name) {
+		$field_prop = '-'.$field_name;
+		if (isset($this->$field_prop)) {
+			return $this->$field_prop;
+		}
+
+		$this->$field_prop = Timber::get_posts($this->meta($field_name));
+		return $this->$field_prop;
 	}
 }
