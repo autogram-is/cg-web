a92fa71b1811f18b898fbffa7ba00425792aae07
Jeff Eaton <jeff@eaton.fyi>
2025-01-13 15:17:17 -0600

    Updates to the business hierarchy migration

--- public_html/wp-content/plugins/cg-migrate/cli/create-hierarchy.php
+++ public_html/wp-content/plugins/cg-migrate/cli/create-hierarchy.php
@@ -1,113 +1,183 @@
 <?php
 
 //  1. Build out Region, Office, Sector, and Service skeleton
-function cg_cli_build_hierarchy($dry_run = false, $preserve = false, $lipsum = false) {
-  // Import the portfolio-hierarchy.csv data
-  $lorem = new Badcow\LoremIpsum\Generator();
+function cg_cli_build_hierarchy($dry_run = false, $preserve = false) {
+  _populate_regions($dry_run, $preserve);
+  _populate_sectors($dry_run, $preserve);
+  _populate_services($dry_run, $preserve);
+  _populate_offices($dry_run, $preserve);
+}
 
-  $datalines = file(CG_MIGRATE_DATA_DIR . '/portfolio-hierarchy.csv');
-  $header = str_getcsv($datalines[0]);
+function _populate_regions($dry_run = false) {
+  $items = load_migration_csv('regions.csv');
 
-  // We accumulate ready-to-save posts into this array. It's less efficient than
-  // doing them one at a time, but this dataset isn't that large and it lets us
-  // sort them for dependencies if necessary.
-  $posts = array();
+  if ($dry_run) {
+    WP_CLI::log("Dry-Run: ". count($items) . " regions defined");
+  } else {
+    foreach ($items as $item) {
+      if (!$item['title']) continue;
 
-  $index = 0;
-  foreach ($datalines as $line) {
-    // Parse each line using str_getcsv
-    $data = str_getcsv($line);
-      
-    // Skip empty lines and header row
-    if (empty($data) || $index == 0) {
-      $index++;
-      continue;
+      $parent = $item['zone'] ? term_exists($item['zone'], 'region') : NULL;
+      $parent_id = $parent ? $parent['term_id'] : NULL;
+  
+      $term_id = wp_insert_term(
+        $item['title'],
+        'region',
+        array(
+          'slug' => $item['slug'],
+          'parent' => $parent_id
+        )
+      );
+
+      WP_CLI::log("Created region tag $term_id (". $item['title'] . ")");
     }
-    
-    $row = array_combine($header, $data);		
+  }
+}
 
-    // Prepare post data
+function _populate_sectors($dry_run = false, $preserve = false) {
+  // Load all the children of the existing 'Sectors' and 'EU Sectors' landing pages
+  $old_sectors = get_all_child_pages(15041);
+  $old_eu_sectors = get_all_child_pages(57642);
+
+  if ($dry_run) {
+    WP_CLI::log("Dry-Run: ". (count($old_sectors) + count($old_eu_sectors)) ." legacy Sectors found");
+  } else if (!$preserve) {
+    foreach ($old_sectors as $post) {
+      wp_delete_post($post->ID, true);
+      WP_CLI::log("Deleted legacy Sector $post->ID");
+    }
+    foreach ($old_eu_sectors as $post) {
+      wp_delete_post($post->ID, true);
+      WP_CLI::log("Deleted legacy Sector $post->ID");
+    }
+  }
+
+  $items = load_migration_csv('sectors.csv');
+
+  foreach($items as $item) {
+    if (!$item['title']) continue;
+
     $post_data = array(
-      'ID'            => $row['id'],
-      'post_type'     => $row['type'],
-      'post_title'    => sanitize_text_field($row['title']),
-      'post_name'     => sanitize_text_field($row['slug']),
-      'post_parent'   => $row['parent'], // If this is a title, we search for parent ID
-      'region'        => empty($row['region']) ? NULL : $row['region'],
-      'locale'        => empty($row['locale']) ? NULL : $row['locale'],
+      'post_type'     => 'cg_sector',
+      'post_title'    => sanitize_text_field($item['title']),
+      'post_name'     => sanitize_text_field($item['slug']),
+      'locale'        => empty($item['locale']) ? NULL : $item['locale'],
       'post_status'   => 'publish',
       'post_author'   => get_current_user_id(),
       'post_date'     => current_time('mysql'),
-      'post_content'  => $lipsum ? $lorem->getParagraphMean() : NULL,
     );
 
-    $posts[$index] = $post_data;
-    $index++;
+    if ($dry_run) {
+      WP_CLI::log("Dry-Run: ". $post_data['post_type'] ." '". $post_data['post_title'] . "' created.");
+    } else {
+      $post_id = wp_insert_post($post_data, true);
+      if ($post_id) {
+        // Check $item['region'] and connect to appropriate taxonomy tag
+
+        if ($item['locale']) {
+          update_field('locale', $item['locale'], $post_id);
+        }
+        WP_CLI::log($post_data['post_type'] ." '". $post_data['post_title'] . "' ($post_id) created.");
+      }
+    }
+  }
+}
+
+function _populate_services($dry_run = false, $preserve = false) {
+  // Load all the children of the existing 'Services' landing page
+  $old_services = get_all_child_pages(14996);
+
+  if ($dry_run) {
+    WP_CLI::log("Dry-Run: ". count($old_services) ." legacy Sectors found");
+  } else if (!$preserve) {
+    foreach ($old_services as $post) {
+      wp_delete_post($post->ID, true);
+      WP_CLI::log("Deleted legacy Service $post->ID");
+    }
   }
 
-  foreach ($posts as $post) {
-    if ($post['post_parent']) {
-      $post['post_parent'] = post_exists($post['post_parent'], '', '', $post['post_type']);
-    }
+  $items = load_migration_csv('services.csv');
 
-    $region = $post['region'];
-    $locale = $post['locale'];
-    unset($post['region']);
-    unset($post['locale']);
-
-    $post_data = sanitize_post($post, 'db');
+  foreach($items as $item) {
+    if (!$item['title']) continue;
+    
+    $post_data = array(
+      'post_type'     => 'cg_service',
+      'post_title'    => sanitize_text_field($item['title']),
+      'post_name'     => sanitize_text_field($item['slug']),
+      'locale'        => empty($item['locale']) ? NULL : $item['locale'],
+      'post_status'   => 'publish',
+      'post_author'   => get_current_user_id(),
+      'post_date'     => current_time('mysql'),
+    );
 
     if ($dry_run) {
-      WP_CLI::log("Dry-Run: ". $post['post_type'] ." '". $post['post_title'] . "' built.");
+      WP_CLI::log("Dry-Run: ". $post_data['post_type'] ." '". $post_data['post_title'] . "' created.");
     } else {
-      $post_id = wp_insert_post($post, true);
-
-      // If locales or regions were set for the post, update them here.
-      // Doing this explicitly ensures ACF keeps keeps track of field associations.
+      $post_id = wp_insert_post($post_data, true);
       if ($post_id) {
-        WP_CLI::log($post['post_type'] ." '". $post['post_title'] . "' ($post_id) created.");
+        // Check $item['region'] and connect to appropriate taxonomy tag
 
-        if ($region) {
-          $rid = post_exists($region, '', '', 'cg_region');
-          if ($rid) {
-            update_field('regions', [$rid], $post_id);
+        if ($item['locale']) {
+          update_field('locale', $item['locale'], $post_id);
+        }
+        WP_CLI::log($post_data['post_type'] ." '". $post_data['post_title'] . "' ($post_id) created.");
+      }
+    }
+  }
+}
+
+function _populate_offices($dry_run = false, $preserve = false) {
+  $old_offices = get_all_child_pages(62959);
+  if ($dry_run) {
+    WP_CLI::log("Dry-Run: ". count($old_offices) ." legacy Offices found");
+  } else if (!$preserve) {
+    foreach ($old_offices as $post) {
+      wp_delete_post($post->ID, true);
+      WP_CLI::log("Deleted legacy Office $post->ID");
+    }
+  }
+
+  $items = load_migration_csv('offices.csv');
+
+  foreach($items as $item) {
+    if (!$item['title']) continue;
+
+    $post_data = array(
+      'post_type'     => 'cg_office',
+      'post_title'    => sanitize_text_field($item['title']),
+      'post_name'     => sanitize_text_field($item['slug']),
+      'post_status'   => 'publish',
+      'post_author'   => get_current_user_id(),
+      'post_date'     => current_time('mysql'),
+    );
+
+    if ($dry_run) {
+      WP_CLI::log("Dry-Run: ". $post_data['post_type'] ." '". $post_data['post_title'] . "' created.");
+    } else {
+      $post_id = wp_insert_post($post_data, true);
+      if ($post_id) {
+        if ($item['location']) {
+          update_field('locale', $item['location'], $post_id);
+        }
+        if ($item['email']) {
+          update_field('locale', $item['email'], $post_id);
+        }
+        if ($item['phone']) {
+          update_field('locale', $item['phone'], $post_id);
+        }
+        if ($item['address']) {
+          update_field('locale', $item['address'], $post_id);
+        }
+
+        if($item['region']) {
+          $region_id = $item['region'] ? term_exists($item['region'], 'region') : NULL;
+          if ($region_id) {
+            wp_set_object_terms($post_id, $region_id, 'region');
           }
         }
-        if ($locale) {
-          update_field('locale', $locale, $post_id);
-        }
-      }
-    }
-  }
 
-  // Delete old sector, service, and office posts
-  if (!$preserve) {
-    $sectors = get_all_child_pages(15041);
-    $services = get_all_child_pages(14996);
-    $eu_sectors = get_all_child_pages(57642);
-    $offices = get_all_child_pages(62959);
-
-    if ($dry_run) {
-      WP_CLI::log("Dry-Run: ". count($sectors) ." legacy Sectors found");
-      WP_CLI::log("Dry-Run: ". count($services) + count($eu_sectors) ." legacy Sectors found");
-      WP_CLI::log("Dry-Run: ". count($offices) ." legacy Offices found");
-
-    } else {
-      foreach ($sectors as $post) {
-        wp_delete_post($post->ID, true);
-        WP_CLI::log("Deleted legacy Sector $post->ID");
-      }
-      foreach ($eu_sectors as $post) {
-        wp_delete_post($post->ID, true);
-        WP_CLI::log("Deleted legacy Sector $post->ID");
-      }
-      foreach ($services as $post) {
-        wp_delete_post($post->ID, true);
-        WP_CLI::log("Deleted legacy Service $post->ID");
-      }
-      foreach ($offices as $post) {
-        wp_delete_post($post->ID, true);
-        WP_CLI::log("Deleted legacy Office $post->ID");
+        WP_CLI::log($post_data['post_type'] ." '". $post_data['post_title'] . "' ($post_id) created.");
       }
     }
   }
