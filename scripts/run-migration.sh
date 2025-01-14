6cc2332eb9d44f8bd4747df2f61639ce874772eb
Jeff Eaton <jeff@eaton.fyi>
2025-01-14 13:33:50 -0600

    Adding scripts folder with localhost and migration commands

---
+++ scripts/run-migration.sh
@@ -0,0 +1,5 @@
+wp cg archive --allow-root
+wp cg hierarchy --allow-root
+wp cg projects --allow-root
+wp cg events --allow-root
+wp cg tags --allow-root
---
+++ scripts/setup-localhost.sh
@@ -0,0 +1,8 @@
+wp user create admin team@autogram.is --role=administrator --allow-root
+wp user update admin --user_pass="admin" --skip-email --allow-root
+
+wp option set siteurl http://localhost --allow-root
+wp option set home http://localhost --allow-root
+wp search-replace https://wordpress-243686-4130740.cloudwaysapps.com http://localhost --skip-columns=guid --allow-root
+
+wp cache flush --allow-root
