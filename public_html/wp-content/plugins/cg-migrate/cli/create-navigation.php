c595e2b5e6bd65e91f88ccb34c55e906bf184bdb
Jeff Eaton <jeff@eaton.fyi>
2025-01-14 11:07:59 -0600

    Stubbing navigation builder

---
+++ public_html/wp-content/plugins/cg-migrate/cli/create-navigation.php
@@ -0,0 +1,7 @@
+<?php
+
+//  1. Build out Primary, Primary-EU, and Footer nav menus
+//  2. Build list landing pages for key indexes
+function cg_cli_build_navigation($dry_run = false, $preserve = false) {
+  
+}
