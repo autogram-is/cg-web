0586f610df16f266a5df8bb7c3e1705a277ec795
Jeff Eaton <jeff@eaton.fyi>
2025-02-03 15:08:50 -0600

    Add the ability to create category-specific page templates

--- public_html/wp-content/themes/cumminggroup/single.php
+++ public_html/wp-content/themes/cumminggroup/single.php
@@ -58,11 +58,24 @@ if ($timber_post->post_type === 'project') {
 	if (count($relationships) > 0) {
 		$context['relationships'] = $relationships;
 	}
+}
 
+$templates = array(
+	'single-' . $timber_post->ID . '.twig',
+	'single-' . $timber_post->post_type . '.twig',
+	'single-' . $timber_post->slug . '.twig',
+	'single.twig'
+);
+
+// Allow category-specific templates, just in case.
+$categories = get_the_category($post->ID);
+if (count($categories) > 0) {
+	$context['news_category'] = $categories[0];
+	array_unshift($templates, 'single-' . $timber_post->post_type . '-' . $categories[0]->slug . '.twig');
 }
 
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
-	Timber::render( array( 'single-' . $timber_post->ID . '.twig', 'single-' . $timber_post->post_type . '.twig', 'single-' . $timber_post->slug . '.twig', 'single.twig' ), $context );
+	Timber::render($templates, $context);
 }
