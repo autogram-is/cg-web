92bb34c5abe985c44b21f205506eea2fa225bac8
Jeff Eaton <jeff@eaton.fyi>
2025-02-10 13:13:55 -0600

    Tag and Form migration work

---
+++ public_html/wp-content/plugins/cg-migrate/cli/create-contact-forms.php
@@ -0,0 +1,9 @@
+<?php
+
+function cg_cli_delete_existing_forms($dry_run = false) {
+  // Backup and remove all existing gravity forms data
+}
+
+function cg_cli_create_new_forms($dry_run = false) {
+  // Can't be done via PHP yet; must import these manually.
+}
--- public_html/wp-content/plugins/cg-migrate/cli/migrate-tags.php
+++ public_html/wp-content/plugins/cg-migrate/cli/migrate-tags.php
@@ -3,9 +3,9 @@
  * The tag migration map takes existing tags and categories from
  * a number of taxonomies and does one of the following:
  * 
- * - Remaps an individual taxo term to a new one (UNTAG)
+ * - Remaps an individual taxo term to a new one (RETAG)
  * - Remaps an individual taxo term to a relationship with another post (RELATE)
- * - Removes the taxo term from the post (RETAG)
+ * - Removes the taxo term from the post (UNTAG)
  * - Removes the taxo term and archives the post (ARCHIVE)
  */
 function cg_map_old_tags($ids = [], $dry_run = false, $preserve = false) {
@@ -14,8 +14,8 @@ function cg_map_old_tags($ids = [], $dry_run = false, $preserve = false) {
 
   // If the post being processed is one of these types, the relationships that replace the
   // term links should be post-type-specific. ie, $post_meta['sectors'], $post_meta['services']
-  // and so on. Other post types have an all-purpose $post_meta['related'] relationship.
-  $portfolio_types = ['project', 'sector', 'service', 'cg_region', 'office'];
+  // and so on. Other post types have an all-purpose $post_meta['related_portfolio_items'] relationship.
+  $portfolio_types = ['project', 'sector', 'service', 'office'];
 
   foreach ($ids as $post_id) {
     $post = get_post($post_id);
@@ -34,36 +34,58 @@ function cg_map_old_tags($ids = [], $dry_run = false, $preserve = false) {
       // be removed or replaced on this particular post.
       foreach ($post_terms as $term) {
         if (key_exists($term->term_id, $map)) {
-          $record = $map[$term->term_id];
+          $record = $map[$term->term_id] ?? NULL;
 
-          if ($record && $record['action'] === 'REPLACE') {
-            $target_field_name = 'related';
-
-            if (in_array($post->post_type, $portfolio_types)) {
-              // A little ugly; these relational field names are just un-prefixed, pluralized versions of the post types
-              $target_field_name = str_replace('cg_', '', $map[$term->term_id]['type']) . 's';
+          if ($record) {
+            if ($record['action'] === 'RELATE') {
+              $target_field_name = 'related_portfolio_items';
+              if (in_array($post->post_type, $portfolio_types)) {
+                $target_field_name = $record['type'] . 's';
+              }
+              $to_relate[$target_field_name][] = $record['id'];
+              $to_remove[$record['old']][] = $term->term_id;
+            } else if ($record['action'] === 'RETAG') {
+              $to_add[$record['type']][] = $record['id'];
+              $to_remove[$record['old']][] = $term->term_id;
+            } else if ($record['action'] === 'UNTAG') {
+              $to_remove[$record['old']][] = $term->term_id;
             }
-            $to_add[$target_field_name][$map[$term->term_id]['id']] = $map[$term->term_id]['id'];
-            $to_remove[$term->taxonomy][$term->term_id] = $term->term_id;
-          } else if ($record && $record['action'] === 'REMOVE') {
-            $to_remove[$term->taxonomy][$term->term_id] = $term->term_id;
           }
         }
       }
       
       $relationships_added = 0;
+      $tags_added = 0;
       $tags_removed = 0;
 
-      // Now, make the actual updates for this particular post
-      foreach($to_add as $field => $ids) {
+      // Update relationships
+      foreach($to_relate as $field => $ids) {
         $relationships_added += count($ids);
         if (!$dry_run) {
-          update_field($field, array_values($ids), $post_id);
+          // Get the existing field values and merge in the new ones
+          // to ensure we don't blow stuff away by running this multiple
+          // times after the tags are deleted.
+          $new_ids = get_field($field, $post_id) ?? [];
+          $new_ids = array_merge($new_ids, $ids);
+          $new_ids = array_unique($new_ids);
+          update_field($field, array_values($new_ids), $post_id);
         } else {
           WP_CLI::log("Dry Run: Add " . join(', ', array_values($ids)) . " to $field on '$post->post_title' ($post->post_type $post->ID)");
         }
       }
 
+      // Apply new taxonomy tags
+      foreach($to_relate as $taxonomy => $ids) {
+        $tags_added += count($ids);
+        if (!$dry_run) {
+          // Get the existing field values
+          wp_add_object_terms($post_id, $ids, $taxonomy);
+        } else {
+          WP_CLI::log("Dry Run: Add " . join(', ', array_values($ids)) . " to $field on '$post->post_title' ($post->post_type $post->ID)");
+        }
+      }
+      
+      // Remove old taxonomy tags
       if (!$preserve) {
         foreach($to_remove as $taxonomy => $ids) {
           $tags_removed += count($ids);
