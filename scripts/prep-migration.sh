462e6245bf5efdcab83aeeabd62f1dd2f7822824
Jeff Eaton <jeff@eaton.fyi>
2025-02-10 13:14:13 -0600

    Updating list of enabled/disabled plugins

--- scripts/prep-migration.sh
+++ scripts/prep-migration.sh
@@ -1,5 +1,5 @@
-wp plugin deactivate fusion-builder fusion-core category-ajax-filter-pro custom-post-type-ui styles-and-layouts-for-gravity-forms hreflang-tags-by-dcgws redirection regenerate-thumbnails relevanssi the-events-calendar events-calendar-pro blogvault-real-time-backup insert-headers-and-footers duplicate-post wordpress-seo --allow-root
-wp plugin activate advanced-custom-fields-pro admin-columns-for-acf-fields cg-core cg-migrate query-monitor query-monitor-twig-profile --allow-root
+wp plugin deactivate if-so if-so-geolocation fusion-builder fusion-core category-ajax-filter-pro custom-post-type-ui styles-and-layouts-for-gravity-forms hreflang-tags-by-dcgws redirection regenerate-thumbnails relevanssi the-events-calendar events-calendar-pro blogvault-real-time-backup insert-headers-and-footers duplicate-post wordpress-seo --allow-root
+wp plugin activate cg-core cg-migrate advanced-custom-fields-pro admin-columns-for-acf-fields acf-gravityforms-add-on query-monitor query-monitor-twig-profile --allow-root
 wp theme activate cumminggroup --allow-root
 
 wp post delete $(wp post list --post_type=revision --format=ids --allow-root) --force --allow-root
