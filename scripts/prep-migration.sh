6fcb1abf928bcbbecfe4ed26e10be496c1b5c473
Jeff Eaton <jeff@eaton.fyi>
2025-02-17 13:28:25 -0600

    Quoted strings and migration script update

--- scripts/prep-migration.sh
+++ scripts/prep-migration.sh
@@ -1,4 +1,4 @@
-wp plugin deactivate if-so if-so-geolocation fusion-builder fusion-core category-ajax-filter-pro custom-post-type-ui styles-and-layouts-for-gravity-forms hreflang-tags-by-dcgws redirection regenerate-thumbnails relevanssi the-events-calendar events-calendar-pro blogvault-real-time-backup insert-headers-and-footers duplicate-post wordpress-seo --allow-root
+wp plugin deactivate if-so if-so-geolocation fusion-builder fusion-core category-ajax-filter-pro custom-post-type-ui styles-and-layouts-for-gravity-forms events-calendar-pro fusion-builder fusion-core buzzsprout-podcasting custom-post-type-ui category-ajax-filter-pro styles-and-layouts-for-gravity-forms if-so if-so-geolocation ifso-audience-self-selection-form-extension the-events-calendar events-calendar-pro --allow-root
 wp plugin activate cg-core cg-migrate advanced-custom-fields-pro admin-columns-for-acf-fields acf-gravityforms-add-on query-monitor query-monitor-twig-profile --allow-root
 wp theme activate cumminggroup --allow-root
 
--- scripts/run-migration.sh
+++ scripts/run-migration.sh
@@ -1,8 +1,9 @@
-wp cg hierarchy --allow-root
-wp cg projects --allow-root
-wp cg posts --allow-root
-wp cg events --allow-root
-wp cg pages --allow-root
-wp cg tags --allow-root
-wp cg navigation --allow-root
-wp cg import --allow-root
+rm ./migration.log
+wp cg hierarchy --allow-root 2>&1 | tee migration.log
+wp cg projects --allow-root 2>&1 | tee migration.log
+wp cg posts --allow-root 2>&1 | tee migration.log
+wp cg events --allow-root 2>&1 | tee migration.log
+wp cg pages --allow-root 2>&1 | tee migration.log
+wp cg tags --allow-root 2>&1 | tee migration.log
+wp cg navigation --allow-root 2>&1 | tee migration.log
+wp cg import --allow-root 2>&1 | tee migration.log
