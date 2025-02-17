1a74c12dea4f63c0b5d75d0071e3a5f2970f646e
Jeff Eaton <jeff@eaton.fyi>
2025-02-17 17:24:32 -0600

    Assume US number if no international prefix is given

--- public_html/wp-content/plugins/cg-core/util/format-telephone.php
+++ public_html/wp-content/plugins/cg-core/util/format-telephone.php
@@ -4,13 +4,18 @@ use Brick\PhoneNumber\PhoneNumberParseException;
 use Brick\PhoneNumber\PhoneNumberFormat;
 
 function cg_format_phone(?string $input, string $format = 'display') {
+    if (str_starts_with($input, '+')) {
+      $country = NULL;
+    } else {
+      $country = 'US';
+    }
     try {
-      $number = PhoneNumber::parse($input);
+      $number = PhoneNumber::parse($input, $country);
       if ($number->isValidNumber()) {
         if ($format === 'url') {
           return $number->format(PhoneNumberFormat::RFC3966);
         } else if ($format === 'local') {
-          return $number->format(PhoneNumberFormat::NATIONAL);
+          return $number->format(PhoneNumberFormat::NATIONAL) . " <!-- " . $number->getRegionCode() . " -->";
         } else {
           return $number->format(PhoneNumberFormat::INTERNATIONAL) . " <!-- " . $number->getRegionCode() . " -->";
         }
