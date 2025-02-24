f7c9787f8e31787e6c31864992fbcc77312cc9dc
Jeff Eaton <jeff@eaton.fyi>
2025-02-24 00:11:46 -0600

    Adding upstatement/routes for portfolio pages

--- public_html/wp-content/themes/cumminggroup/vendor/composer/autoload_static.php
+++ public_html/wp-content/themes/cumminggroup/vendor/composer/autoload_static.php
@@ -34,6 +34,10 @@ class ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
         ),
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -61,9 +65,24 @@ class ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
         ),
+        'Composer\\Installers\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'R' => 
+        array (
+            'Routes' => 
+            array (
+                0 => __DIR__ . '/..' . '/upstatement/routes',
+            ),
+        ),
     );
 
     public static $classMap = array (
+        'AltoRouter' => __DIR__ . '/..' . '/altorouter/altorouter/AltoRouter.php',
         'CURLStringFile' => __DIR__ . '/..' . '/symfony/polyfill-php81/Resources/stubs/CURLStringFile.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'ReturnTypeWillChange' => __DIR__ . '/..' . '/symfony/polyfill-php81/Resources/stubs/ReturnTypeWillChange.php',
@@ -74,6 +93,7 @@ class ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit45ee886cc44b90a50dfa9a43cb90e5d0::$classMap;
 
         }, null, ClassLoader::class);
