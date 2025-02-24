081bacf8779e30a0a6d8a1465d4d44836cb24ce8
Jeff Eaton <jeff@eaton.fyi>
2025-02-24 16:29:11 -0600

    Fresh pass at event listing; separate pages for upcoming and past

--- public_html/wp-content/themes/cumminggroup/src/block-hooks.php
+++ public_html/wp-content/themes/cumminggroup/src/block-hooks.php
@@ -272,125 +272,161 @@ add_filter('block_type_metadata', function($metadata) {
  * @return array
  */
 function cg_block_type_variations($variations, $block_type ) {
-
 	if ( 'namespace/block-a' === $block_type->name ) {
 		// Add variations to the `$variations` array.
 	} elseif ( 'namespace/block-b' === $block_type->name ) {
 		// Add more variations to the `$variations` array.
 	}
-
 	return $variations;
 }
 
 
 /**
- * Register custom block type variations.
+ * Intercept data population for custom Cumming Group listing blocks,
+ * populating them with the content selected in the block.
+ * 
+ * Context variables include:
+ * 
+ * - post_id:    The ID of the WP post whose content includes the block.
+ * - slug:       The block's internal identifier
+ * - is_preview: TRUE if the block is being previewed in the content editor
+ * - fields:     The ACF fields attached to the block in question
+ * - block:      The Wordpress Block instance, including internal variables etc.
+ * - classes:    CSS classes to be attached to the block's wrapper element.
  *
  * @param array    $context array of Twig context variables.
  * @return array
  */
 function cg_populate_custom_block_data($context) {
-  $post_id =  $context['post_id'];
-  $slug =  $context['slug'];
-  $is_preview =  $context['is_preview'];
-  $fields =  $context['fields'];
-  $block =  $context['block'];
-  $classes = $context['classes'];
+  $func = str_replace(['/', '-'], '_', $context['slug']) . "_block_data";
+  if (function_exists($func)) {
+    return $func($context);
+  } else {
+    return $context;
+  }
+}
 
-  $posts = [];
+/**
+ * Our most complex listing block.
+ * 
+ * It's capable of listing events in three ways:
+ * 
+ * - All events
+ * - Upcoming events
+ * - Past events
+ */
+function cg_list_events_block_data($context) {
+  extract($context, EXTR_SKIP);
 
-  if ('cg-list-events' === $slug) {
-    // Build past or future event list, populate $context['posts']
-    if ($fields['window'] === 'upcoming') {
-      $query_options = array(
-        'meta_query' => array(
-          array(
-            'key' => 'start_date',
-            'value' => current_time('mysql'),
-            'compare' => '>=',
-            'type' => 'DATE'
-          )
-        ),
-        'post_type'     => 'event',
-        'posts_per_page' => $fields['limit'] ?? 10,
-        'orderby' => 'start_date'
-      );
-    } else {
-      $query_options = array(
-        'meta_query' => array(
-          array(
-            'key' => 'start_date',
-            'value' => current_time('mysql'),
-            'compare' => '<=',
-            'type' => 'DATE'
-          )
-        ),
-        'post_type'     => 'event',
-        'posts_per_page' => $fields['limit'] ?? 20,
-        'orderby' => 'start_date'
-      );
-      if ($filds['pagination'] ?? false) {
-        $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
-      }
-    }
-    $context['posts'] = Timber::get_posts($query_options);
-    
-  } else if ('cg-list-news' === $slug) {
-    // Build news, optionally filtered by category to, to populate $context['posts']
-    $query_options = array(
-      'post_type' => 'post',
-      'posts_per_page' => isset( $fields['limit'] ) && $fields['limit'] > 0 ? $fields['limit'] : 10,
-      'cat' => $fields['categories'] ?? NULL
-    );
-    if ($fields['pagination']) {
-      $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
-    }
-    $context['posts'] = Timber::get_posts($query_options);
+  $all_query = array(
+    'meta_query' => array(
+      array('key' => 'start_date')
+    ),
+    'post_type'      => 'event',
+    'posts_per_page' => $fields['limit'] ?? 10,
+    'orderby'        => 'start_date'
+  );
 
-  } else if ('cg-list-reports' === $slug) {
-    // Build news, optionally filtered by category to, to populate $context['posts']
-    $query_options = array(
-      'post_type' => 'report',
-      'posts_per_page' => isset( $fields['limit'] ) && $fields['limit'] > 0 ? $fields['limit'] : 10,
-    );
-    if ($fields['pagination']) {
-      $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
-    }
-    $context['posts'] = Timber::get_posts($query_options);
+  $upcoming_query = array(
+    'meta_query' => array(
+      array(
+        'key'     => 'start_date',
+        'value'   => current_time('mysql'),
+        'compare' => '>=',
+        'type'    => 'DATE'
+      )
+    ),
+    'post_type'      => 'event',
+    'posts_per_page' => $fields['limit'] ?? 10,
+    'orderby'        => 'start_date'
+  );
 
-  } else if ('cg-list-team' === $slug) {
-    // Build people, falling back on current post's related people, to populate $context['posts']
-    $context['posts'] = $posts;
+  $past_query = array(
+    'meta_query' => array(
+      array(
+        'key'     => 'start_date',
+        'value'   => current_time('mysql'),
+        'compare' => '<=',
+        'type'    => 'DATE'
+      )
+    ),
+    'post_type'      => 'event',
+    'posts_per_page' => $fields['limit'] ?? 20,
+    'orderby'        => 'start_date'
+  );
 
-  } else if ('cg-list-projects' === $slug) {
-    // Build projects to populate $context['posts']
-    $context['posts'] = $posts;
-
-  } else if ('cg-list-regions' === $slug) {
-    // Build office list for the selected region and its zub-regions
-    foreach ($fields['regions'] as $item) {
-      $region_ids = get_term_children($item['region'], 'region');
-      $region_ids[] = $item['region'];
-
-      $query_options = array(
-        'post_type' => 'office',
-        'posts_per_page' => -1,
-        'tax_query' => array(
-          array(
-            'taxonomy' => 'region',
-            'field' => 'id',
-            'terms' => $region_ids,
-            'operator' => 'IN',
-          )
-        )
-      );
-
-      $context['regions'][] = array(
-        'heading' => $item['heading'] ?? '',
-        'posts' => Timber::get_posts($query_options),
-      );
-    }
+  if ($fields['pagination'] ?? false) {
+    $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
   }
 
+  if ($fields['window'] == 'all') {
+    $context['posts'] = Timber::get_posts($all_query);
+  } else if ($fields['window'] == 'upcoming') {
+    $context['posts'] = Timber::get_posts($upcoming_query);
+  } else if ($fields['window'] == 'past') {
+    $context['posts'] = Timber::get_posts($past_query);
+  }
+
+  return $context;
+}
+
+
+function cg_list_news_block_data($context) {
+  extract($context, EXTR_SKIP);
+
+  // Build news, optionally filtered by category to, to populate $context['posts']
+  $query_options = array(
+    'post_type' => 'post',
+    'posts_per_page' => isset( $fields['limit'] ) && $fields['limit'] > 0 ? $fields['limit'] : 10,
+    'cat' => $fields['categories'] ?? NULL
+  );
+  if ($fields['pagination']) {
+    $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
+  }
+  $context['posts'] = Timber::get_posts($query_options);
+  return $context;
+}
+
+function cg_list_reports_block_data($context) {
+  extract($context, EXTR_SKIP);
+
+  // Build a list of recent reports and return it in $context['posts']
+  $query_options = array(
+    'post_type' => 'report',
+    'posts_per_page' => isset( $fields['limit'] ) && $fields['limit'] > 0 ? $fields['limit'] : 10,
+  );
+  if ($fields['pagination']) {
+    $query_options['paged'] = get_query_var('paged') ? get_query_var('paged') : 1;
+  }
+  $context['posts'] = Timber::get_posts($query_options);
+  return $context;
+}
+
+function cg_list_regions_block_data($context) {
+  extract($context, EXTR_SKIP);
+  $posts = [];
+
+  foreach ($fields['regions'] as $item) {
+    $region_ids = get_term_children($item['region'], 'region');
+    $region_ids[] = $item['region'];
+
+    $query_options = array(
+      'post_type' => 'office',
+      'posts_per_page' => -1,
+      'tax_query' => array(
+        array(
+          'taxonomy' => 'region',
+          'field' => 'id',
+          'terms' => $region_ids,
+          'operator' => 'IN',
+        )
+      )
+    );
+
+    $context['regions'][] = array(
+      'heading' => $item['heading'] ?? '',
+      'posts' => Timber::get_posts($query_options),
+    );
+  }
   return $context;
 }
